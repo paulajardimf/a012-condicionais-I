d95d80cbebb55f44a1a33a33f157421c11450a71
Paula <paulajardimf@gmail.com>
2022-09-06 13:43:40 -0300

    exercícios finalizados

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,22 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = prompt("Qual a sua idade?");
+let terminouEnsinoMedio = confirm("Você terminou o ensino médio?");
+let cursandoFaculdade = confirm("Você está cursando alguma faculdade?");
 
-if(){
 
+if(idade >= 18){
+    console.log("Você atingiu a maior idade.");
 }else{
-	
+	console.log("Você é menor de idade.");
+}
+
+if(terminouEnsinoMedio) {
+    console.log("Você terminou o ensino médio.");
+}else{
+    console.log("Você não terminou o ensino médio");
+}
+
+if(cursandoFaculdade) {
+    console.log("Você está cursando a faculdade.");
+}else{
+    console.log("Você não está cursando a faculdade.");
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,8 @@
-let nacionalidade //= adicione o prompt aqui
+const nacionalidade = prompt("Escreva aqui a sua nacionalidade:");
+
+
+if (nacionalidade.toLowerCase() === "brasileira" || nacionalidade.toLowerCase() === "argentina" || nacionalidade.toLowerCase() === "uruguaia" || nacionalidade.toLowerCase() === "chilena" || nacionalidade.toLowerCase() === "colombiana") {
+    console.log(`Sua nacionalidade é ${nacionalidade.toLowerCase()}.`);
+} else {
+    console.log("Nacionalidade não encontrada.");
+}
