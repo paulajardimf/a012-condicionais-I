5b211c9e507cf596fd0fbfc5a1e3d856f444c816
Paula <paulajardimf@gmail.com>
2022-09-06 18:29:15 -0300

    exercício corrigido

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,8 +1,8 @@
-const nacionalidade = prompt("Escreva aqui a sua nacionalidade:");
+const nacionalidade = prompt("Escreva aqui a sua nacionalidade:").toLowerCase();
 
 
-if (nacionalidade.toLowerCase() === "brasileira" || nacionalidade.toLowerCase() === "argentina" || nacionalidade.toLowerCase() === "uruguaia" || nacionalidade.toLowerCase() === "chilena" || nacionalidade.toLowerCase() === "colombiana") {
-    console.log(`Sua nacionalidade é ${nacionalidade.toLowerCase()}.`);
+if (nacionalidade === "brasileira" || nacionalidade === "argentina" || nacionalidade === "uruguaia" || nacionalidade === "chilena" || nacionalidade === "colombiana") {
+    console.log(`Sua nacionalidade é ${nacionalidade}.`);
 } else {
     console.log("Nacionalidade não encontrada.");
 }
